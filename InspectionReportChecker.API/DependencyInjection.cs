a44d21f79e53bba35608800073e1c7035686f88d
kaytl <kaytl@DESKTOP-TI4LR7A>
2025-02-21 11:40:01 +0000

    Add project files.

---
+++ InspectionReportChecker.API/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace InspectionReportChecker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        //add dependencies n shit here
+        //e.g.
+        //services.AddScoped<IRepository, Repository>();
+
+        return services;
+    }
+}
---
+++ InspectionReportChecker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace InspectionReportChecker.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        //add dependencies n shit here
+        //e.g. adding ef framework and dependencies
+        return services;
+    }
+}
